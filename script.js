7a882862dfdb6d41d4e348c3b1600a33644ba678
kofithebass <kofi@kofihayford.com>
2021-01-01 17:23:01 -0500

    Added conditional logic
    Added conditional If, else if, and else statements for the password length

--- script.js
+++ script.js
@@ -4,18 +4,36 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var passwordText = document.querySelector("#password");
-  passwordText.innerHTML = "Lorem ipsum test text";
+  passwordText.innerHTML = password
   var password = generatePassword();
-  passwordText.value = password;
+//   passwordText.value = password;
 }
 
-function generatePassword() {
+function generatePassword(password) { 
   var pwLength = prompt("Length");
-  // console.log(pwLength);
+
+//   console.log(pwLength);
+  var passwordText = document.querySelector("#password");
   var upperCase = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
   var lowerCase = "abcdefghijklmnopqrstuvwxyz";
   var specialCharacters = "!@#$%^&*()-_+=";
   var numbers = "1234567890";
+  
+  //   password = specialCharacters + upperCase + lowerCase + numbers;
+
+  passwordText.innerHTML = pwLength
+  // check for PW length and check for correct number of characters.
+  if(pwLength < 8){
+      alert("Your password is too short")
+    }
+  else if (pwLength > 128){
+      alert("Your password is too long")
+    }
+  else{
+    for (i=0; i<=pwLength; i++){ 
+        passsword = 
+    }
+  }
 }
 
 //take the value of PW length and use that to inform the length of the password
