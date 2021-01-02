e0e86d40d24229e00f349751706a7b999ae7e0d1
kofithebass <kofi@kofihayford.com>
2021-01-02 15:02:16 -0500

    made overall structural and organizational changes to the code
    Grouped important and major variables at the beginning. Everything is essentially grouped into the writePassword function.
    Confirm prompts only execute once the password length is validated. A series of if statements follow to certify which conditions/parameters the user wants in the password. I used console logs to see what the results are at 2 different stages of the codes execution.  I learnt about the Join function and decided to use that function to put the generated password properly.

--- script.js
+++ script.js
@@ -1,37 +1,39 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Write password to the #password input. Grouped all variables together for clarity and succingtness. 
 function writePassword() {
-  var passwordText = document.querySelector("#password");
-  passwordText.innerHTML = password
-  var password = generatePassword();
-}
-
-//Add all other prompts to the list of questions to capture criteria
-function generatePassword(password) { 
   var pwLength = prompt("Characeter Length of Password");
-  var caseUpper = confirm("Uppercase Y/N");
-  var caseLower = confirm("Lowercase Y/N");
-  var caseNumber = confirm("Numbers Y/N");
-  var caseSpecial = confirm("Special Symbols or Characters Y/N");
-
-  // List of more variables and characters 
   var passwordText = document.querySelector("#password");
   var upperCase = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
   var lowerCase = "abcdefghijklmnopqrstuvwxyz";
   var specialCharacters = "!@#$%^&*()-_+=";
   var numbers = "1234567890";
-  var password = "" 
+  var password = "" ;
   
+  // I moved the rest of the code logic lower becuase the rest of the code is really relevant until we force the right password length condiditions. 
+
   // check for PW length and check for correct number of characters. 
-  if(pwLength < 8){
-      alert("Your password is too short")
-    }
-  else if (pwLength > 128){
-      alert("Your password is too long")
-    }
+  if (pwLength < 8) {
+    alert("Your password is too short");
+    // prevent the rest of the code from running if password length is too short
+    return;
+  } else if (pwLength > 128) {
+    alert("Your password is too long");
+    // prevent the rest of the code from running if password lenth is too long
+    return;
+  }
   else{
+
+  // NOW that the password length is acceptable, THEN proceed with the rest of the code. 
+
+  //display prompts to confirm the desired parameters  
+  var caseUpper = confirm("Uppercase Y/N");
+  var caseLower = confirm("Lowercase Y/N");
+  var caseNumber = confirm("Numbers Y/N");
+  var caseSpecial = confirm("Special Symbols or Characters Y/N");
+
+  // if the user presses 'Ok' on each popup, we add that parameter into the password string to be processed
   if (caseUpper) {
        password = password.concat(upperCase);
   }
@@ -46,23 +48,31 @@ function generatePassword(password) {
   }
   console.log("pass1", password)
 
-    // Comment out our older for loop to test new idea 
-    // for (i = 0; i <= pwLength - 1; i++){ 
-    //     password = password + upperCase.charAt(
-    //         Math.floor(Math.random() * Math.floor(upperCase.length))
-    //     );   
-    // }
-    
-    // Changing the For Loop and adding using a new variable 
+
+    // Changing the For Loop and adding using a new variable with an empty array
     var passwordCharacters = []
+
+    // Use a for loop to loop through the password string and pick any random character
+    // then store the selected character into an array
+
     for (i = 0; i <= pwLength - 1; i++){ 
         password = passwordCharacters + password.charAt(
                  Math.floor(Math.random() * Math.floor(password.length))
              );  
-    } 
+      // store each randomCharacter into the array using push method
+    passwordCharacters.push(randomCharater);
   }
-  passwordText.innerHTML = password
-  console.log("pass2", password)
+
+  console.log("pswd2: ", passwordCharacters);
+
+  // combine the different array characters into one string using 'join'"
+  console.log("pswd3: ", passwordCharacters.join(""));
+
+  // write the new password on the #password input
+  passwordText.innerHTML = passwordCharacters.join("");
 }
+ 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// function generatePassword(password) {}. Seemed to be causing issues. After doing some research and discussing it with a mentor, it seems we could doo without it. 
